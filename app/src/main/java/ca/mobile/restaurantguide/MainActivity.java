5e809a7e38c3758a38fb206ee9f02af81da6eccf
DevarshiKSagar <devarshi.sagar@gmail.com>
2020-11-14 15:53:26 +0530

    add restaurant activity design

--- app/src/main/java/ca/mobile/restaurantguide/MainActivity.java
+++ app/src/main/java/ca/mobile/restaurantguide/MainActivity.java
@@ -2,13 +2,28 @@ package ca.mobile.restaurantguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.goToAddRes);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addRestaurantActivity();
+            }
+        });
+    }
+    public void addRestaurantActivity(){
+        Intent intent = new Intent(this, add_restaurants.class);
+        startActivity(intent);
+
     }
 }
