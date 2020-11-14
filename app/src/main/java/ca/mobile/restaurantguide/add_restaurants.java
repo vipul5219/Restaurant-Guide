49e2b16a971a9c4c7e2890a2fd3f541e8ec53a00
DevarshiKSagar <devarshi.sagar@gmail.com>
2020-11-14 14:25:59 +0530

    main activity design

---
+++ app/src/main/java/ca/mobile/restaurantguide/add_restaurants.java
@@ -0,0 +1,14 @@
+package ca.mobile.restaurantguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_restaurants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_restaurants);
+    }
+}
