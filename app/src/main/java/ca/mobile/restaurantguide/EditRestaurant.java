399b1a32e558aaa866a64dc561b2c07aa4b3fcba
DevarshiKSagar <devarshi.sagar@gmail.com>
2020-11-14 16:42:18 +0530

    edit restaurant detais activity design

---
+++ app/src/main/java/ca/mobile/restaurantguide/EditRestaurant.java
@@ -0,0 +1,14 @@
+package ca.mobile.restaurantguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditRestaurant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_restaurant);
+    }
+}
